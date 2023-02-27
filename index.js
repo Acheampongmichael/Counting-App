034fa98680db04d3f3cf327193115de9bab3bb6e
Acheampongmichael <80366178+Acheampongmichael@users.noreply.github.com>
2023-02-27 01:02:59 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+//document.getElementById("count-el").innerText = 5
+
+let count = 0 
+
+let countEl = document.getElementById("count-el")
+let saveEl = document.getElementById("save-el")
+
+function increment() {
+    count += 1
+    countEl.textContent = count
+}
+
+//increment()
+
+function save() {
+    let countStr = count + " - "
+    saveEl.textContent += countStr
+    countEl.textContent = 0
+    count = 0
+}
+
+//save()
+
+
+
